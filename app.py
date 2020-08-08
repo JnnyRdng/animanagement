27d055cddfb8f04891103dff2b6471b9f37e9574
Jonny Reading <jonnyreading@gmail.com>
2020-08-08 18:01:57 +0100

    fleshed out base.html ready to start work on controllers

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 
+from controllers.vet_controller import vet_blueprint
+
 app = Flask(__name__)
+
+app.register_blueprint(vet_blueprint)
 
 
 @app.route("/")
 def homepage():
-    return "flask is running"
+    return render_template("index.html", title="Home")
 
 
 if __name__ == "__main__":
