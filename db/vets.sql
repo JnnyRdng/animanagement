5a4a26be7d65e84446b0bde35dd9cb80547b083f
Jonny Reading <jonnyreading@gmail.com>
2020-08-10 15:08:22 +0100

    updated database tables to use TIMESTAMP instead of VARCHAR for dates

--- db/vets.sql
+++ db/vets.sql
@@ -30,18 +30,18 @@ CREATE TABLE owners (
 CREATE TABLE animals (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    dob VARCHAR(255),
+    dob TIMESTAMP,
     species VARCHAR(255),
     breed VARCHAR(255),
     owner_id INT REFERENCES owners(id),
     vet_id INT REFERENCES vets(id),
-    date_registered VARCHAR(255),
+    date_registered TIMESTAMP,
     checked_in BOOLEAN
 );
 
 CREATE TABLE records (
     id SERIAL PRIMARY KEY,
-    date VARCHAR(255),
+    date TIMESTAMP,
     entry TEXT,
     animal_id INT REFERENCES animals(id) ON DELETE CASCADE
 );
@@ -60,18 +60,18 @@ INSERT INTO vets (first_name, last_name) VALUES ('Sarah', 'Jackson');
 INSERT INTO vets (first_name, last_name) VALUES ('Hugh', 'Polson');
 INSERT INTO vets (first_name, last_name) VALUES ('James', 'Anderson');
 
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Fluff', '14-04-2018', 'Cat', 'Maine Coon', 1, 1, '06-08-2020', false);
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Floof', '23-07-2017', 'Dog', 'Spaniel', 2, 2, '06-08-2020', true);
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Adrian', '01-06-2010', 'Chameleon', 'Veiled Chameleon', 1, 2, '05-08-2020', true);
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Sticky', '19-08-2016', 'Stick Insect', 'Common', 3, 4, '05-08-2020', true);
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Howard', '12-01-1965', 'Tortoise', 'Galapagos', 3, 3, '07-08-2020', true);
-INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Harambe', '27-05-1999', 'Gorilla', 'Western Lowland', 2, 5, '09-08-2020', true);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Fluff', '2018-04-14', 'Cat', 'Maine Coon', 1, 1, '2020-08-06', false);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Floof', '2017-07-23', 'Dog', 'Spaniel', 2, 2, '2020-08-06', true);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Adrian', '1990-10-23', 'Chameleon', 'Veiled Chameleon', 1, 2, '2020-08-05', true);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Sticky', '2019-11-28', 'Stick Insect', 'Common', 3, 4, '2020-08-05', true);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Howard', '1965-01-12', 'Tortoise', 'Galapagos', 3, 3, '07-08-2020', true);
+INSERT INTO animals (name, dob, species, breed, owner_id, vet_id, date_registered, checked_in) VALUES ('Harambe', '1999-05-27', 'Gorilla', 'Western Lowland', 2, 5, '2016-05-28', false);
 
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Cat is sick', 1);
-INSERT INTO records (date, entry, animal_id) VALUES ('08-08-2020', 'Cat is still sick', 1);
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Dog is ill', 2);
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Chameleon can''t change colour', 3);
-INSERT INTO records (date, entry, animal_id) VALUES ('03-08-2020', 'We''ve had Sticky in for a few days now and something is wrong, he''s been right off his food and he''s hardly moving at all. Further tests are definitely needed but this could be serious.', 4);
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Further tests show that Sticky is in fact a stick and we''ve all made a terrible mistake', 4);
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Poked him with a stick (haha). He''s definitely a stick.', 4);
-INSERT INTO records (date, entry, animal_id) VALUES ('07-08-2020', 'Harambe got shot and we''re all very sad about it', 5);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-07 10:29:50', 'Cat is sick', 1);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-08 14:11:01', 'Cat is still sick', 1);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-07 15:34:11', 'Dog is ill', 2);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-07 16:11:40', 'Chameleon can''t change colour', 3);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-03 11:21:37', 'We''ve had Sticky in for a few days now and something is wrong, he''s been right off his food and he''s hardly moving at all. Further tests are definitely needed but this could be serious.', 4);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-07 17:54:28', 'Further tests show that Sticky is in fact a stick and we''ve all made a terrible mistake', 4);
+INSERT INTO records (date, entry, animal_id) VALUES ('2020-08-07 17:58:22', 'Poked him with a stick (haha). He''s definitely a stick.', 4);
+INSERT INTO records (date, entry, animal_id) VALUES ('2016-05-28 18:09:44', 'Harambe got shot and we''re all very sad about it', 5);
